789431226d82b4bc5b59e293c4ded1366fd29717
displaycoffee <adria.m.murphy@gmail.com>
2022-04-13 20:58:27 -0600

    trying to fix async function

--- assets/src/js/components/filters/Filters.jsx
+++ assets/src/js/components/filters/Filters.jsx
@@ -7,7 +7,43 @@ import { filterList } from '../../scripts/filterList';
 import { utils } from '../../scripts/utils';
 
 const Filters = () => {
-	return 'Testing';
+	let [filters, setFilters] = useState('');
+	//let filters = filterList;
+	useEffect(() => {
+		buildFilters();
+
+		//return null;
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+	async function buildFilters() {
+		let droppedValues = [];
+
+		cardList.forEach((card) => {
+			if (card.dropped) {
+				if (!droppedValues.includes(card.dropped)) {
+					droppedValues.push(card.dropped);
+					filterList.dropped.values.push({
+						value: card.dropped,
+						count: 1,
+					});
+				}
+			}
+		});
+
+		setFilters(filterList);
+	}
+
+	console.log('tjese filters', filters);
+
+	return (
+		filters.dropped && (
+			<div>
+				{filters.dropped.values.map((value) => {
+					return <p>{value.value}</p>;
+				})}
+			</div>
+		)
+	);
 };
 
 export default Filters;
